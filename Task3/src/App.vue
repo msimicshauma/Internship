0d667159d44720773be00d268c900e16d9454344
msimicshauma <milos.simic95@gmail.com>
2019-01-18 23:57:50 +0100

    added task-3

---
+++ Task3/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+body {
+  background-color: #fff;
+  margin: 0;
+  color: #888;
+  font-family: Ubuntu;
+}
+
+h1 {
+  margin: 0;
+  margin-bottom: 10px;
+}
+
+p {
+  margin: 0;
+}
+</style>
